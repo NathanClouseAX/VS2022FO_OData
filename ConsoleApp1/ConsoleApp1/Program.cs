fd1341a9a608f7a2466adfd1bf47997898eb0970
Nathan Clouse <nathan.clouse@atomicax.com>
2022-11-27 22:55:39 -0500

    initial commit of F&O OData proxies using VS2022

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ConsoleApp1
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
